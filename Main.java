874ecc2867f1143091634e91cd6ca307b215107e
edidudedo <eduardoiglesius101@gmail.com>
2024-11-25 18:43:47 +0900

    Add improvements

--- Main.java
+++ Main.java
@@ -1,37 +1,42 @@
+// IMPROVEMENTS
+/// (DONE) Change the reduce Health from O(n2) of TC to O(n) of SC 
+/// (DONE) Add Setting for this attributes : 
+//// GridSize
+//// Player1 mark
+//// Player2 mark
+/// (Done) No need to print the grid when the input is wrong
+/// (Done) tile.getValue should be TileVal and not String (String should only be during the print
+/// (Done) Change output from O(n2) to O(1)
+
 import java.util.Scanner;
 
 class Main{
     public static void main(String args[]){
+        // Initialization 
+        /// TC = O(n2)
+        /// SC = O(n2)
         TicTacToe tictactoe = new TicTacToe();
         Scanner scanner = new Scanner(System.in);
+
+        // PrintRule
+        /// TC = O(1)
+        /// SC = O(1)
         printRule();
-        int winner = 0;
-        do{
-            tictactoe.printGrid();
-            int curPlayerIndex = tictactoe.getCurPlayer();
-            System.out.println("Player " + curPlayerIndex + " move : ");
-            String userInput = scanner.nextLine();
-            int[] inputs = checkUserInput(tictactoe, userInput);
 
-            if(inputs == null) {
-                System.out.println("Wrong input");
-                continue;
-            }
+        // Setting
+        /// TC = O(l + n2)
+        /// SC = O(n2)
+        setting(tictactoe, scanner);
 
-            if(!tictactoe.curPlayerPlay(inputs[0], inputs[1])){
-                System.out.println("Tile has already been filled");
-                continue;
-            }
+        // Game
+        /// TC = O(k(n+l))
+        /// SC = O(n)
+        int winner = game(tictactoe, scanner);
 
-            tictactoe.switchPlayers();
-            winner = tictactoe.checkWonPlayer();
-            if(winner==0){
-                tictactoe.reduceHealth();
-            }
-        }while(winner == 0);
-
-        tictactoe.printGrid();
-        System.out.println("Congratulations player " + winner + "!");
+        // Ending
+        /// TC = O(1)
+        /// SC = O(1)
+        ending(tictactoe, winner);
         scanner.close();
         return;
     }
@@ -41,6 +46,94 @@ class Main{
         System.out.println("After you did several moves, the last move you had will be removed");
         System.out.println("The first person who makes a line horizontally, vertically, or diagonally will win");
         System.out.println("");
+    }
+
+    static private void setting(TicTacToe tictactoe, Scanner scanner){
+        do{
+            System.out.println("Set the Setting (Input format : GRIDSIZE Player1Mark Player2Mark) : ") ;
+            String settingInput = scanner.nextLine(); 
+
+            if(checkUserSettingInput(tictactoe, settingInput)){
+                break;
+            }
+        }while(true);
+    }
+
+    static private boolean checkUserSettingInput(TicTacToe tictactoe, String input){
+        int gridSize = 0;
+        char player1Mark = 'X';
+        char player2Mark = 'O';
+        String[] words = input.trim().split("\\s+");
+        if(words.length != 3){
+            System.out.println("Wrong Number of Inputs");
+            return false;
+        }
+        try{
+            gridSize = Integer.parseInt(words[0]);
+        }catch(Exception e){
+            System.out.println("Grid size is not an integer");
+            return false;
+        }
+        if(gridSize < 3 || gridSize > 5){
+            System.out.println("Grid size should be 3 to 5");
+            return false;
+        } 
+        if(words[1].length() != 1) {
+            System.out.println("Player 1 mark is not a char");
+            return false;
+        }
+        player1Mark = words[1].charAt(0);
+        if(player1Mark == '#'){
+            System.out.println("Player 1 mark cannot be default mark #");
+            return false;
+        }
+
+        if(words[2].length() != 1) {
+            System.out.println("Player 2 mark is not a char");
+            return false;
+        }
+        player2Mark = words[2].charAt(0);
+        if(player2Mark == '#'){
+            System.out.println("Player 2 mark cannot be default mark #");
+            return false;
+        }
+
+        tictactoe.changeGridSize(gridSize);
+        tictactoe.changePlayerMark(0, player1Mark);
+        tictactoe.changePlayerMark(1, player2Mark);
+        return true;
+    }
+
+    static private int game(TicTacToe tictactoe, Scanner scanner){
+        int winner = 0;
+        do{
+            tictactoe.printGridSB(); // O(1)
+            int curPlayerIndex = tictactoe.getCurPlayer(); // O(1)
+
+            // TC = O(l)
+            do{
+                System.out.println("Player " + curPlayerIndex + " move : ");
+                String userInput = scanner.nextLine();
+                int[] inputs = checkUserInput(tictactoe, userInput);
+    
+                if(inputs == null) {
+                    System.out.println("Wrong input");
+                }
+                else {
+                    if(!tictactoe.curPlayerPlay(inputs[0], inputs[1])){
+                        System.out.println("Tile has already been filled");
+                    }
+                    else break;
+                }            
+            } while(true);
+
+            winner = tictactoe.checkWonPlayer(); // O(n)
+            if(winner==0){
+                tictactoe.switchPlayers(); // O(1)
+                tictactoe.checkBoard(); // O(1)
+            }
+        }while(winner == 0);
+        return winner;
     }
 
     static private int[] checkUserInput(TicTacToe tictactoe, String input){
@@ -62,8 +155,11 @@ class Main{
         if(ret[0] < 0 || ret[0] >=  tictactoe.getGridSize() || ret[1] < 0 || ret[1] >= tictactoe.getGridSize()){
             return null;
         }
-
         return ret;
-
+    }
+    
+    static void ending(TicTacToe tictactoe, int winner){
+        tictactoe.printGridSB();
+        System.out.println("Congratulations player " + winner + "!");
     }
 }
